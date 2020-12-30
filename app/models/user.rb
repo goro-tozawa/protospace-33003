e2699b04ceb49bb460e722f72ade42ebe0bc90d1
goro-tozawa <ralugoro.0905@icloud.com>
2020-12-30 11:54:19 +0900

    空のcommit2

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :email, presence: true
   validates :password, presence: true
+  validates :name, presence: true
 
   # has_many :comments
   has_many :prototypes
